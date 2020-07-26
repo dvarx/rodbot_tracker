ea4958830e5efa99f8a63d8b9734035690e576b5
dvarx <dvarx@ethz.ch>
2020-07-26 22:56:10 +0200

    added gui support for changing parameters

---
+++ basic_gui.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 12:21:39 2020
+
+@author: dvarx
+"""
+
+from PyQt5.QtWidgets import QApplication,QLabel,QWidget,QVBoxLayout,QPushButton,QHBoxLayout,QSlider,QMainWindow
+from PyQt5.QtCore import Qt
+import multiprocessing as mp
+
+app=QApplication([])
+
+"""
+Global Variables
+"""
+send_end_thres=0
+
+class MainWindow(QMainWindow):
+    def __init__(self,send_end_thres_,*args,**kwargs):
+        super(MainWindow,self).__init__(*args,**kwargs)
+
+        self.send_end_thres=send_end_thres_
+
+        self.setWindowTitle("Control")
+        self.layout=QVBoxLayout()
+
+        self.toplayout=QHBoxLayout()
+        self.bottomlayout=QHBoxLayout()
+
+        self.topbutton=QPushButton("Top")
+        self.toplabel=QLabel("Top-Button: ")
+        self.toplayout.addWidget(self.toplabel)
+        self.toplayout.addWidget(self.topbutton)
+
+        self.bottombutton=QPushButton("Bottom")
+        self.bottomlabel=QLabel("Bottom-Button: ")
+        self.bottomlayout.addWidget(self.bottomlabel)
+        self.bottomlayout.addWidget(self.bottombutton)
+
+        self.sliderlabel=QLabel("Current Value:%-3d"%(150))
+        self.slider=QSlider(Qt.Horizontal)
+        self.slider.setTickPosition(QSlider.TicksBelow)
+        self.slider.setTickInterval(10)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(255)
+        self.slider.setValue(150)
+        self.slider.valueChanged.connect(self.slider_val_chgd)
+        self.sliderlayout=QVBoxLayout()
+        self.sliderlayout.addWidget(self.slider)
+        self.sliderlayout.addWidget(self.sliderlabel)
+
+        self.topbutton.clicked.connect(self.top_btn_cb)
+
+        self.layout.addLayout(self.toplayout)
+        self.layout.addLayout(self.bottomlayout)
+        self.layout.addLayout(self.sliderlayout)
+
+        self.mainwidget=QWidget()
+        self.mainwidget.setLayout(self.layout)
+        self.setCentralWidget(self.mainwidget)
+
+    def top_btn_cb(self):
+        self.topbutton.setText("Clicked Top")
+
+    def slider_val_chgd(self,val):
+        self.sliderlabel.setText("Value changed:%-3d"%(val))
+        self.send_end_thres.send(val)
+
+    def set_send_pipe(self,send_end_thres_):
+        self.send_end_thres=send_end_thres_
+
+
+def main_loop(send_end_thres_):
+    mainwindow=MainWindow(send_end_thres_)
+    mainwindow.show()
+
+    app.exec_()
--- basictracker.py
+++ basictracker.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 from time import sleep
+import multiprocessing as mp
+from basic_gui import main_loop
 
 """
 Created on Thu Jul 23 17:00:57 2020
@@ -10,73 +12,103 @@ How to use:
 
 @author: dvarx
 """
+if __name__=="__main__":
+  """
+  Global Definitions
+  """
+  roi_upperleft_pos=np.array([300,180])
+  alpha=0.5
+  LOWER_THRES=100
+  AOI_LENGTH=50
+  loop_video=True
 
-#upper-left corner of AOI
-roi_upperleft_pos=np.array([300,180])
-alpha=0.5
-LOWER_THRES=100
-AOI_LENGTH=50
+  """
+  Initialization Code
+  """
+  cap = cv2.VideoCapture('test1.m4v')
 
-cap = cv2.VideoCapture('test1.m4v')
+  #mouse callback function, we need it to reset the running_avf when the user clicks the screen
+  def mouse_cb(event,x,y,flags,param):
+      global roi_upperleft_pos
+      if event==cv2.EVENT_LBUTTONDOWN:
+          print("CLicked : (%d,%d)"%(x,y))
+          roi_upperleft_pos=np.array([max(0,x-LOWER_THRES/2),max(0,y-LOWER_THRES/2)])
 
-#mouse callback function, we need it to reset the running_avf when the user clicks the screen
-def mouse_cb(event,x,y,flags,param):
-    global roi_upperleft_pos
-    if event==cv2.EVENT_LBUTTONDOWN:
-        print("CLicked : (%d,%d)"%(x,y))
-        roi_upperleft_pos=np.array([max(0,x-LOWER_THRES/2),max(0,y-LOWER_THRES/2)])
+  #define pipes for communication with GUI process
+  (recv_end_thres,send_end_thres)=mp.Pipe()
+  proc_gui=mp.Process(target=main_loop,args=[send_end_thres])
+  sleep(1)
+  proc_gui.start()
 
-#define named window for mouse cb
-cv2.namedWindow("viewing_window")
-cv2.setMouseCallback('viewing_window',mouse_cb)
+  #define named window for mouse cb
+  cv2.namedWindow("viewing_window")
+  cv2.setMouseCallback('viewing_window',mouse_cb)
 
-# Check if camera opened succesfully
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
+  # Check if camera opened succesfully
+  if (cap.isOpened()== False): 
+    print("Error opening video stream or file")
 
-
-while(cap.isOpened()):
-  ret, frame = cap.read()
-  #sleep a bit to slow down the framerate
-  sleep(0.2)
-  if ret == True:
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    appliedthresh,imgmask=cv2.threshold(gray,LOWER_THRES,255,cv2.THRESH_BINARY)
-    imgmask=cv2.bitwise_not(imgmask)
-    
-    maskaoi=imgmask[int(roi_upperleft_pos[1]):int(roi_upperleft_pos[1])+AOI_LENGTH,int(roi_upperleft_pos[0]):int(roi_upperleft_pos[0])+AOI_LENGTH]
-    moments=cv2.moments(maskaoi)
-    if(moments["m00"]!=0):
-        dpos=np.array([moments["m10"]/moments["m00"],moments["m01"]/moments["m00"]])
-        center_position_abs=roi_upperleft_pos+dpos.T
-        cv2.circle(frame,(int(center_position_abs[0]),int(center_position_abs[1])),color=(1,1,1),radius=5)
-        roi_upperleft_pos=center_position_abs-np.array([AOI_LENGTH/2,AOI_LENGTH/2])
-
-    
-    #draw aoi
-    frame=cv2.rectangle(frame,(int(roi_upperleft_pos[0]),int(roi_upperleft_pos[1])), \
-                        (int(roi_upperleft_pos[0]+AOI_LENGTH),int(roi_upperleft_pos[1]+AOI_LENGTH)),color=(0,0,255))
+  """
+  Main Program Loop
+  """
+  framecounter=0
+  while(cap.isOpened()):
+    ret, frame = cap.read()
+    framecounter+=1
+    #sleep a bit to slow down the framerate
+    #sleep(0.2)
+    #check if threshold has been adjusted in GUI
+    if recv_end_thres.poll():
+      LOWER_THRES=recv_end_thres.recv()
+    if ret == True and loop_video == True:
+      #check if end of video reached, if yes repeat it
+      if framecounter==cap.get(cv2.CAP_PROP_FRAME_COUNT):
+          framecounter=0
+          cap.set(cv2.CAP_PROP_POS_FRAMES,0)
         
-    imgmask_rgb=cv2.cvtColor(imgmask,cv2.COLOR_GRAY2RGB)
-    
-    imgmask_rgb=cv2.rectangle(imgmask_rgb,(int(roi_upperleft_pos[0]),int(roi_upperleft_pos[1])), \
-                    (int(roi_upperleft_pos[0]+AOI_LENGTH),int(roi_upperleft_pos[1]+AOI_LENGTH)),color=(0,0,255))
-        
-    #concatenate images and show
-    concatimage=np.concatenate((frame,imgmask_rgb),axis=1)
+      #apply threshold and mask
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      appliedthresh,imgmask=cv2.threshold(gray,LOWER_THRES,255,cv2.THRESH_BINARY)
+      imgmask=cv2.bitwise_not(imgmask)
+      
+      #update center of mass and aoi position
+      maskaoi=imgmask[int(roi_upperleft_pos[1]):int(roi_upperleft_pos[1])+AOI_LENGTH,int(roi_upperleft_pos[0]):int(roi_upperleft_pos[0])+AOI_LENGTH]
+      moments=cv2.moments(maskaoi)
+      if(moments["m00"]!=0):
+          dpos=np.array([moments["m10"]/moments["m00"],moments["m01"]/moments["m00"]])
+          center_position_abs=roi_upperleft_pos+dpos.T
+          cv2.circle(frame,(int(center_position_abs[0]),int(center_position_abs[1])),color=(1,1,1),radius=5)
+          roi_upperleft_pos=center_position_abs-np.array([AOI_LENGTH/2,AOI_LENGTH/2])
 
-    cv2.imshow('viewing_window',concatimage)
+      
+      #draw aoi
+      frame=cv2.rectangle(frame,(int(roi_upperleft_pos[0]),int(roi_upperleft_pos[1])), \
+                          (int(roi_upperleft_pos[0]+AOI_LENGTH),int(roi_upperleft_pos[1]+AOI_LENGTH)),color=(0,0,255))
+          
+      imgmask_rgb=cv2.cvtColor(imgmask,cv2.COLOR_GRAY2RGB)
+      
+      imgmask_rgb=cv2.rectangle(imgmask_rgb,(int(roi_upperleft_pos[0]),int(roi_upperleft_pos[1])), \
+                      (int(roi_upperleft_pos[0]+AOI_LENGTH),int(roi_upperleft_pos[1]+AOI_LENGTH)),color=(0,0,255))
+          
+      #concatenate images and show
+      concatimage=np.concatenate((frame,imgmask_rgb),axis=1)
 
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+      cv2.imshow('viewing_window',concatimage)
+
+      # Press Q on keyboard to  exit
+      if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+    # Break the loop
+    else: 
       break
 
-  # Break the loop
-  else: 
-    break
 
-# When everything done, release the video capture object
-cap.release()
+  """
+  Exit Program
+  """
+  # When everything done, release the video capture object
+  cap.release()
 
-# Closes all the frames
-cv2.destroyAllWindows()
+  # Closes all the frames
+  cv2.destroyAllWindows()
